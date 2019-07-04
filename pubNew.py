9e5eaa7deb55eddfe257df7a4847ab29aec51dc0
ivanpbf <ivan27rlb@gmail.com>
2019-07-04 16:40:09 -0400

    cambios ligeros al sub

---
+++ pubNew.py
@@ -0,0 +1,178 @@
+import ssl
+import sys
+import random
+import time
+import psycopg2
+import paho.mqtt.client
+import paho.mqtt.publish
+import datetime
+import numpy as np
+import json
+
+myConnection = psycopg2.connect(host = 'localhost', user= 'postgres',password ='pass123', dbname= 'Proyecto1BD') #estos son los mios
+
+def on_connect(client, userdata, flags, rc):
+	print('connected')
+
+def main():
+	client = paho.mqtt.client.Client("Sambil", False)
+	client.on_connect = on_connect
+	client.qos = 0 			
+	client.connect(host='localhost')
+	cur = myConnection.cursor()
+
+	start_dt = datetime.date.today().replace(day=1, month=1).toordinal()
+	end_dt = datetime.date.today().toordinal()
+	random_day = datetime.date.fromordinal(random.randint(start_dt, end_dt))	
+	dia = random_day.day
+	mes = random_day.month
+
+	fechaI = datetime.datetime.now().replace(month=mes,day=dia) #la hora entrante, falta el dia
+	while(True):
+		entrante = int(np.random.uniform(1,21)) #Pueden entrar cualquier cantidad de personas por comodidad pongamos que sea de 1 a 20
+
+		for i in range(entrante):					# Genera los datos para las "n" personas que entraron [0,n]
+
+			acceso = int(np.random.uniform(1,4))  #camara de acceso o puerta
+			horaAccesoE = fechaI.replace(hour=int(np.random.uniform(8,18)),minute=int(np.random.uniform(0,60)), second=int(np.random.uniform(0,60)))
+			auxHoraultima = horaAccesoE #esta la usare para marcar la ultima actividad
+
+			#creamos la cedula para la persona
+			cedula = int(np.random.uniform(1,251)) #limite de 250 usuarios que pueden existir
+			cur.execute('SELECT * FROM "rasgos" WHERE "cedula" = %s', [cedula])
+			persona = cur.fetchone()
+			seEncontro = False
+			if persona is None:
+				detectoRasgos =  np.random.rand()		# Random para ver si la camara pudo detectar los rasgos de la persona (edad,sexo)
+				if(detectoRasgos > 0.6):
+					sexoAux = int(np.random.uniform(0,2))
+					edad = int(np.random.uniform(12,81))
+					if sexoAux == 0:
+						sexo = "F"
+					else:
+						sexo = "M"
+					payloadR = {
+						"cedula": cedula,
+						"edad": edad,
+						"sexo": sexo 	
+					}
+					client.publish('Sambil/rasgos',json.dumps(payloadR), qos=0)
+				else:
+					payloadNR = {
+						"cedula": cedula
+					}
+					client.publish('Sambil/rasgos',json.dumps(payloadNR), qos = 0) #publica los rasgos que consiguio, cedula siempre se "consigue"
+			else:
+				seEncontro = True
+				cur.execute('SELECT edad FROM "rasgos" WHERE "cedula" = %s', [cedula])
+				edadr = cur.fetchone()
+				if edadr is None:
+					detectoRasgos =  np.random.rand()		# Random para ver si la camara pudo detectar los rasgos de la persona, ya que ha pasado antes, incrementa el chance
+					if(detectoRasgos > 0.5):
+						sexoAux = int(np.random.uniform(0,2))
+						edad = int(np.random.uniform(12,61))	
+						if sexoAux == 0:
+							sexo = "F"
+						else:
+							sexo = "M"
+						#update de los datos que se encontraron ahora si
+						cur.execute('UPDATE rasgos SET "edad" = %s, "sexo" = %s WHERE "cedula" = %s;', (edad, sexo, cedula))
+			tieneMAC = None
+			if seEncontro:
+				cur.execute('SELECT mac FROM "usuario" WHERE "cedula" = %s', [cedula])
+				macaux = cur.fetchone()
+				if macaux is None:
+					tieneTelefono = np.random.rand()		#tal vez ahora tendra un telefono
+					if(tieneTelefono > 0.4):
+						mac = int(np.random.uniform(0,9999999))
+						tieneMAC = True
+						#pupdate
+						cur.execute('UPDATE usuario SET "mac" = %s WHERE "cedula" = %s;', (mac, cedula))
+				else:
+					mac = macaux[0]
+					tieneMAC = True
+			else:
+				tieneTelefono = np.random.rand()		#Random para ver si la persona tiene telefono
+				if(tieneTelefono > 0.3):
+					mac = int(np.random.uniform(0,9999999))
+					tieneMAC = True
+					payloadU = {
+						"mac": mac,
+						"cedula": cedula
+					}
+					client.publish('Sambil/usuario',json.dumps(payloadU), qos=0)
+			
+			#los metodos de compra y tiendas
+			entroTienda = np.random.rand()
+			if(entroTienda > 0.5):
+				for j in range(int(np.random.uniform(1,11))):
+					idTienda = int(np.random.uniform(1,11))
+					dateEntrada = auxHoraultima + datetime.timedelta(hours=np.random.uniform(int(np.random.uniform(auxHoraultima.hour)),18), minutes=np.random.uniform(auxHoraultima.minute,60), seconds=np.random.uniform(0,60))
+					dateSalida = dateEntrada + datetime.timedelta(minutes=np.random.uniform(2,31))
+					auxHoraultima = dateSalida
+					payloadT = {
+						"idtienda": idTienda,
+						"hentrada": str(dateEntrada),
+						"hsalida": str(dateSalida)
+					}
+					client.publish('Sambil/torniquete',json.dumps(payloadT), qos=0)
+
+					compro = np.random.rand()
+					if(compro > 0.4):
+						monto = int(np.random.uniform(1,1000))
+						if tieneMAC == True:
+							payloadV = {
+								"monto": monto,
+								"idtienda": idTienda,
+								"cedula": cedula,
+								"mac": mac
+							}
+							client.publish('Sambil/venta',json.dumps(payloadV), qos=0)
+						else:
+							payloadV = {
+								"monto": monto,
+								"idtienda": idTienda,
+								"cedula": cedula
+							}
+							client.publish('Sambil/venta',json.dumps(payloadV), qos=0)
+			
+			#vamos a la feria
+			fueFeria = np.random.rand()
+			if(fueFeria > 0.5):
+				idmesa = int(np.random.uniform(1,11))
+				fechaOcupada = auxHoraultima + datetime.timedelta(hours=np.random.uniform(int(np.random.uniform(auxHoraultima.hour)),18), minutes=np.random.uniform(auxHoraultima.minute,60), seconds=np.random.uniform(0,60))
+				fechaDesocupada = fechaOcupada + datetime.timedelta(minutes=np.random.uniform(2,60))
+				auxHoraultima = fechaDesocupada
+
+				if(tieneMAC):
+					payloadM = {
+						"nmesa": idmesa,
+						"fechaocupada": str(fechaOcupada),
+						"fechadesocupada": str(fechaDesocupada),
+						"mac": mac
+					}
+					client.publish('Sambil/mesa',json.dumps(payloadM), qos=0)
+				else:
+					payloadMU = {
+						"nmesa": idmesa,
+						"fechaocupada": str(fechaOcupada),
+						"fechadesocupada": str(fechaDesocupada)
+					}	
+					client.publish('Sambil/mesa',json.dumps(payloadMU), qos=0)
+
+			horaAccesoS = auxHoraultima + datetime.timedelta(hours=np.random.uniform(int(np.random.uniform(auxHoraultima.hour)),18), minutes=np.random.uniform(auxHoraultima.minute,60), seconds=np.random.uniform(0,60))
+			salida = int(np.random.uniform(1,4)) #lo mismo que la entrada
+			payloadA = {
+				"entrada": acceso,
+				"salida": salida,
+				"horaacceso": str(horaAccesoE),
+				"horasalida": str(horaAccesoS),
+				"cedula": cedula
+			}
+			client.publish('Sambil/acceso',json.dumps(payloadA), qos=0)
+			time.sleep(1)
+
+		fechaI += datetime.timedelta(days = 1)
+	
+if __name__ == '__main__':
+	main()
---
+++ subNew.py
@@ -0,0 +1,109 @@
+import ssl
+import sys
+import psycopg2
+import paho.mqtt.client
+import json
+import time
+import datetime
+import numpy as np
+
+#estos son los mios
+conAccesos = psycopg2.connect(host = 'localhost', user= 'postgres',password ='pass123', dbname= 'SambilAccesos')
+conVentas = psycopg2.connect(host = 'localhost', user= 'postgres',password ='pass123', dbname= 'SambilVentas')
+conMesas = psycopg2.connect(host = 'localhost', user= 'postgres',password ='pass123', dbname= 'SambilMesas')
+
+def insertarRasgos(r):
+    cur = conAccesos.cursor()
+    if r.get('edad') is None:
+        cur.execute("INSERT INTO rasgos (cedula) VALUES (%s)", (r["cedula"],)) #INSERTAR EN LA TABLA rasgos
+    else:
+        cur.execute("INSERT INTO rasgos (cedula, edad, sexo) VALUES (%s, %s, %s);", (r["cedula"], r["edad"], r["sexo"])) #INSERTAR EN LA TABLA rasgos
+    conAccesos.commit()
+
+def insertarUsuario(u):
+    cur = conAccesos.cursor()
+    cur.execute("INSERT INTO usuario (mac, cedula) VALUES (%s,%s);", (u["mac"],u["cedula"])) #INSERTAR EN LA TABLA usuario
+    conAccesos.commit()
+
+def insertarTorniquete(t):
+    cur = conAccesos.cursor()
+    cur.execute("INSERT INTO torniquete (idtienda, hentrada, hsalida) VALUES (%s,%s,%s);", (t["idtienda"],t["hentrada"], t["hsalida"])) #INSERTAR EN LA TABLA torniquete
+    conAccesos.commit()
+
+def insertarVenta(v):
+    cur = conVentas.cursor()
+    if v.get('mac') is None:
+        cur.execute("INSERT INTO venta (monto, idtienda, cedula) VALUES (%s,%s,%s);", (v["monto"], v["idtienda"], v["cedula"])) #INSERTAR EN LA TABLA venta
+    else:
+        cur.execute("INSERT INTO venta (monto, idtienda, cedula, mac) VALUES (%s,%s,%s,%s);", (v["monto"], v["idtienda"], v["cedula"], v["mac"])) #INSERTAR EN LA TABLA venta
+    conVentas.commit()
+
+def insertarMesa(m):
+    cur = conMesas.cursor()
+    if m.get('mac') is None:
+        cur.execute("INSERT INTO mesa (nmesa, fechaocupada, fechadesocupada) VALUES (%s,%s,%s);", (m["nmesa"], m["fechaocupada"], m["fechadesocupada"])) #INSERTAR EN LA TABLA mesa
+    else:
+        cur.execute("INSERT INTO mesa (nmesa, fechaocupada, fechadesocupada, mac) VALUES (%s,%s,%s,%s);", (m["nmesa"], m["fechaocupada"], m["fechadesocupada"], m["mac"])) #INSERTAR EN LA TABLA mesa
+    conMesas.commit()
+
+def insertarAcceso(a):
+    cur = conAccesos.cursor()
+    cur.execute("INSERT INTO acceso (entrada, salida, horaacceso, horasalida, cedula) VALUES (%s,%s,%s,%s,%s);", (a["entrada"], a["salida"], a["horaacceso"], a["horasalida"], a["cedula"])) #INSERTAR EN LA TABLA acceso
+    conAccesos.commit()
+
+def on_connect(client, userdata, flags, rc):    
+    print('Conectado ID (%s)' % client._client_id)
+    client.subscribe(topic='Sambil/#', qos=0)    
+
+def on_message(client, userdata, message):   
+    r = json.loads(message.payload.decode('utf-8'))
+    print(r)  
+    print('------------------------------')     
+    insertarRasgos(r)
+
+def on_message3(client, userdata, message):   
+    u = json.loads(message.payload.decode('utf-8'))
+    print(u) 
+    print('------------------------------')     
+    insertarUsuario(u)
+
+def on_message4(client, userdata, message):   
+    t = json.loads(message.payload.decode('utf-8'))
+    print(t) 
+    print('------------------------------')     
+    insertarTorniquete(t)
+
+def on_message5(client, userdata, message):   
+    v = json.loads(message.payload.decode('utf-8'))
+    print(v) 
+    print('------------------------------')     
+    insertarVenta(v)
+
+def on_message7(client, userdata, message):   
+    m = json.loads(message.payload.decode('utf-8'))
+    print(m) 
+    print('------------------------------')     
+    insertarMesa(m)
+
+def on_message9(client, userdata, message):   
+    a = json.loads(message.payload.decode('utf-8'))
+    print(a) 
+    print('------------------------------')     
+    insertarAcceso(a)
+
+def main():	
+    client = paho.mqtt.client.Client()
+    client.on_connect = on_connect
+    client.message_callback_add('Sambil/rasgos',  on_message)
+    client.message_callback_add('Sambil/usuario', on_message3)
+    client.message_callback_add('Sambil/torniquete', on_message4)
+    client.message_callback_add('Sambil/venta', on_message5)
+    client.message_callback_add('Sambil/mesa', on_message7)
+    client.message_callback_add('Sambil/acceso', on_message9)
+    client.connect(host='localhost') 
+    client.loop_forever()
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
+
